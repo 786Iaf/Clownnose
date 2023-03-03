f1bdcd997556788381cd18e89e5e9325ebbc866e
786Iaf <115845673+786Iaf@users.noreply.github.com>
2023-03-03 16:22:46 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+noseX=0;
+noseY=0;
+function preload(){
+    clown_nose=loadImage("https://i.postimg.cc/s2qXy3NZ/Red-Nose-Clown-PNG-Photo-Image.png")
+}
+
+
+function setup(){
+    canvas=createCanvas(500,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(500,500);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function modelLoaded(){
+    console.log("it works");
+
+}
+function take_snapshot(){
+    save("img.png");
+}
+function draw(){
+    image(video,0,0,500,500);
+    image(clown_nose,noseX-15,noseY-15,30,30)
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+noseX=results[0].pose.nose.x;
+noseY=results[0].pose.nose.y;
+        console.log("nose x= "+results[0].pose.nose.x);
+console.log("nose y="+results[0].pose.y);
+    }
+}
